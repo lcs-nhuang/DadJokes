96e36ba61fc7009d9d9854f107c22ae013e1f226
Nicole Huang <nhuang@lcs.on.ca>
2022-02-22 11:20:05 -0500

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Nicole on 2022/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
